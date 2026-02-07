95cd328b13894117798c4830046614d4d1699845
IvaBozovic <ivaa.bozovic@gmail.com>
2026-02-07 17:29:29 +0100

    prepravljen navbar i dodat requireSistemAdmin

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,31 +1,41 @@
 "use client";
-
+ 
 import Link from "next/link";
-import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import Button from "@/components/ui/Button";
-import { useAuth } from "@/context/AuthContext"; 
-
+import { useAuth } from "@/context/AuthContext";
+ 
 export default function Navbar() {
   const router = useRouter();
   const { korisnik, logout } = useAuth();
-
+ 
   const isHr = korisnik?.ulogaId === 2;
-
+  const isAdmin = korisnik?.ulogaId === 1;
+ 
   const handleLogout = async () => {
     await logout();
-    router.replace("/login");  
-    router.refresh();          
+    router.replace("/login");
+    router.refresh();
   };
-
+ 
   return (
     <div className="w-full border-b border-zinc-700 bg-zinc-900">
       <div className="max-w-5xl mx-auto px-6 py-3 flex items-center justify-between">
         <div className="flex gap-2">
-          <Link href="/zaposleni"><Button>Moj profil</Button></Link>
-          {isHr && <Link href="/hr"><Button>HR</Button></Link>}
+          {!isAdmin && (
+            <>
+              <Link href="/zaposleni">
+                <Button>Moj profil</Button>
+              </Link>
+ 
+              {isHr && (
+                <Link href="/hr">
+                  <Button>HR</Button>
+                </Link>
+              )}
+            </>
+          )}
         </div>
-
         <Button className="bg-gray-600" onClick={handleLogout}>
           Logout
         </Button>
---
+++ lib/auth/requireSistemAdmin.ts
@@ -0,0 +1,32 @@
+import { db } from "@/db";
+import { eq } from "drizzle-orm";
+import { korisnik } from "@/db/schema";
+import { getAuthCookie, verifyAuthToken } from "@/lib/auth/jwt";
+ 
+export async function requireSistemAdmin() {
+  const token = await getAuthCookie();
+  if (!token) throw new Error("UNAUTHORIZED");
+ 
+  const payload = await verifyAuthToken(token);
+ 
+  const rows = await db
+    .select({
+      id: korisnik.id,
+      ulogaId: korisnik.ulogaId,
+      statusNaloga: korisnik.statusNaloga,
+    })
+    .from(korisnik)
+    .where(eq(korisnik.id, payload.korisnikId))
+    .limit(1);
+ 
+  if (rows.length === 0) throw new Error("UNAUTHORIZED");
+ 
+  const k = rows[0];
+ 
+  if (!k.statusNaloga) throw new Error("DEACTIVATED");
+ 
+ 
+  if (k.ulogaId !== 1) throw new Error("FORBIDDEN");
+ 
+  return k;
+}
